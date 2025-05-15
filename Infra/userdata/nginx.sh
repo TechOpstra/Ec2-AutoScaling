13e8aa3676f48d789891042a8c3ac02da9486e6c
Harish <107391575+8555936327@users.noreply.github.com>
2025-05-15 21:12:34 +0530

    Update nginx.sh

--- Infra/userdata/nginx.sh
+++ Infra/userdata/nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install nginx -y
+sudo apt-get update -y
+sudo apt-get install nginx -y
 sudo systemctl start nginx
 sudo systemctl enable nginx
 echo "<h1>Welcome to E-Commerce App</h1>" > /var/www/html/index.html
